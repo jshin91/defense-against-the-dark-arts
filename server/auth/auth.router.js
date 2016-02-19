d4943e82b7f48c39686bd3e45e64ef053974ee9c
Jisoo Shin <jisooxshin@gmail.com>
2016-02-19 16:49:17 -0500

    end of pt 2 data theft

--- server/auth/auth.router.js
+++ server/auth/auth.router.js
@@ -4,17 +4,40 @@ var router = require('express').Router();
 
 var HttpError = require('../utils/HttpError');
 var User = require('../api/users/user.model');
+var crypto = require('crypto');
+var iterations = 1;
+var bytes = 64;
+
+var hashedPassword = function(password) {
+	var salt = crypto.randomBytes(16);
+	var buffer = crypto.pbkdf2Sync(password, salt, iterations, bytes);
+	var hash = buffer.toString('base64');
+	// User.findOne()
+	// if(password === )
+	return hash
+}
+
 
 router.post('/login', function (req, res, next) {
-	User.findOne(req.body).exec()
-	.then(function (user) {
-		if (!user) throw HttpError(401);
-		req.login(user, function () {
-			res.json(user);
-		});
+
+	User.findOne(req.body.email)
+	.then(function(user) {
+		if(hashedPassword(user.password) === hashedPassword(req.body.password)) {
+				User.findOne(req.body).exec()
+				.then(function (user) {
+					// if (!user) throw HttpError(401);
+					req.login(user, function () {
+						res.json(user);
+					});
+				})
+			}
+		else {
+			throw HttpError(401);
+		}	
 	})
-	.then(null, next);
-});
+	.then(null, next)
+})
+
 
 router.post('/signup', function (req, res, next) {
 	User.create(req.body)
--- server/index.js
+++ server/index.js
@@ -3,9 +3,19 @@
 var app = require('./app'),
 	db = require('./db');
 
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 var port = 8080;
-var server = app.listen(port, function () {
-	console.log('HTTP server patiently listening on port', port);
-});
+
+var server = https.createServer(options, app).listen(port);	
+
+// var server = app.listen(port, function () {
+// 	console.log('HTTP server patiently listening on port', port);
+// });
 
 module.exports = server;
